302b78ddcb94cb9ae17c23e00a17e08837386100
azeez-abp <adioadeyoriazeez@gmail.com>
2023-08-22 12:58:53 -0700

    0 task re-try

--- _printf.c
+++ _printf.c
@@ -1,42 +1,94 @@
-#include "main.h"
-/**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
- */
-int _printf(const char * const format, ...)
-{
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}
-	};
+#include <unistd.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <stdio.h>
 
+/**
+ * _print_char - helper function to print a character
+ * @args: va_list containing the argument list
+ * @charCount: pointer to the character count
+ */
+void _print_char(va_list args, int *charCount)
+{
+	char c = va_arg(args, int);
+
+	write(1, &c, 1);
+	(*charCount)++;
+}
+
+/**
+ * _print_string - helper function to print a string
+ * @args: va_list containing the argument list
+ * @charCount: pointer to the character count
+ */
+void _print_string(va_list args, int *charCount)
+{
+	const char *str = va_arg(args, const char *);
+
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+		(*charCount)++;
+	}
+}
+
+/**
+ * _switch - function that selects the format specifier
+ * @args: va_list containing the argument list
+ * @format: the format specifier
+ * @charCount: pointer to the character count
+ */
+void _switch(va_list args, char format, int *charCount)
+{
+	switch (format)
+	{
+		case 'c':
+			_print_char(args, charCount);
+			break;
+		case 's':
+			_print_string(args, charCount);
+			break;
+		case '%':
+			write(1, &format, 1);
+			(*charCount)++;
+			break;
+		default:
+			break;
+	}
+}
+
+/**
+ * _printf - custom printf function
+ * @format: the format string
+ * @...: variable number of arguments
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
 	va_list args;
-	int i = 0, j, len = 0;
+	int charCount = 0;
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
 
-Here:
-	while (format[i] != '\0')
+
+	while (*format)
 	{
-		j = 13;
-		while (j >= 0)
+		if (*format == '%')
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
+			format++;
+			_switch(args, *format, &charCount);
 		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		else
+		{
+			write(1, format, 1);
+			charCount++;
+		}
+
+		format++;
 	}
+
 	va_end(args);
-	return (len);
+	return (charCount);
 }
+
--- main.h
+++ main.h
@@ -16,17 +16,7 @@
  *
  */
 
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} convert_match;
 
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int printf_int(va_list args);
-int printf_dec(va_list args);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
